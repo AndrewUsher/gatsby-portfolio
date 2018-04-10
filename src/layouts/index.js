b26409d1e6583bf7f3be9c6160b114c39f31eb64
Andrew Usher <andrewusher00@gmail.com>
2018-04-10 15:48:51 -0500

    :sparkles: Finish home page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -13,16 +13,7 @@ const TemplateWrapper = ({ children, data }) => (
       meta={[{ name: 'description', content: 'Sample' }, { name: 'keywords', content: 'sample, something' }]}
     />
     <Header title={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      }}
-    >
-      {children()}
-    </div>
+    <div>{children()}</div>
   </div>
 )
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,32 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 const IndexPage = () => (
-  <div>
-    
-  </div>
+  <main className="banner">
+    <div className="hero-box">
+      <h2>I am Andrew Usher.</h2>
+      <h3>I'm a front-end developer focused on crafting clean, friendly, and beautiful user experiences.</h3>
+    </div>
+    <div className="social-icons-container">
+      <h3>Where to find me</h3>
+      <div className="social-icons">
+        <a href="https://www.linkedin.com/in/andrew-usher-125332120/" className="linkedin" target="_blank">
+          Linkedin
+        </a>
+        <a href="https://github.com/AndrewUsher" className="github" target="_blank">
+          Github
+        </a>
+        <a href="http://andrewusher.co/blog" className="blog" target="_blank">
+          Blog
+        </a>
+        <a href="https://instagram.com/AndrewUsher" className="instagram" target="_blank">
+          Instagram
+        </a>
+        <a href="https://codepen.io/uncledrew35/" className="codepen" target="_blank">
+          Codepen
+        </a>
+      </div>
+    </div>
+  </main>
 )
 
 export default IndexPage
