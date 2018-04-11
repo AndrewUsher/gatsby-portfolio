8f2769d1c223308fe5389b52c7b472a3346e3a05
Andrew Usher <andrewusher00@gmail.com>
2018-04-11 11:18:45 -0500

    :hammer: Seperate hero box into its own component

---
+++ src/components/HeroBox.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const HeroBox = () => (
+  <div className="hero-box">
+    <h2>I am Andrew Usher.</h2>
+    <h3>I'm a front-end developer focused on crafting clean, friendly, and beautiful user experiences.</h3>
+  </div>
+)
+
+export default HeroBox
