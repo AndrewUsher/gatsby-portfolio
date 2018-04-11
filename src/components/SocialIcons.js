13b46af5e97beb535325eaa44b0ebf29789bf85f
Andrew Usher <andrewusher00@gmail.com>
2018-04-11 11:19:30 -0500

    :hammer: Seperate social icons into own component

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const SocialIcons = () => (
+  <div className="social-icons-container">
+    <h3>Where to find me</h3>
+    <div className="social-icons">
+      <a href="https://www.linkedin.com/in/andrew-usher-125332120/" className="linkedin" target="_blank">
+        Linkedin
+      </a>
+      <a href="https://github.com/AndrewUsher" className="github" target="_blank">
+        Github
+      </a>
+      <a href="http://andrewusher.co/blog" className="blog" target="_blank">
+        Blog
+      </a>
+      <a href="https://instagram.com/AndrewUsher" className="instagram" target="_blank">
+        Instagram
+      </a>
+      <a href="https://codepen.io/uncledrew35/" className="codepen" target="_blank">
+        Codepen
+      </a>
+    </div>
+  </div>
+)
+
+export default SocialIcons
