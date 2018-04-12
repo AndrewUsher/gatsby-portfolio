9908ad674425f5a75060ec1985c257e0def7b966
Andrew Usher <andrewusher00@gmail.com>
2018-04-12 11:33:12 -0500

    Change font to Lato

--- src/html.js
+++ src/html.js
@@ -21,7 +21,7 @@ module.exports = class HTML extends React.Component {
           <meta charSet="utf-8" />
           <meta httpEquiv="x-ua-compatible" content="ie=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-          <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet" />
+          <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
           {this.props.headComponents}
           {css}
         </head>
