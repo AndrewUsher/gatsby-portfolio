7e242028ba22ebe619c374604602553f7190381e
Andrew Usher <andrewusher00@gmail.com>
2018-04-13 18:08:20 -0500

    Add custom 404 page and bump version

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+  <div className="not-found">
+    <div className="not-found-box">
+      <h2>Sorry...</h2>
+      <p>You just found a page that doesn't exist... the sadness...</p>
+      <p>The page you're looking for may have been removed, had its name changed, or may be unavailable.</p>
+      <p>You might find the page you're looking for down below:</p>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
   </div>
 )
 
