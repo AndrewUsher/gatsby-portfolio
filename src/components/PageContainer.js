1d5b1e463bd109890ec7936fb65f286acea9ab7e
Andrew Usher <andrewusher00@gmail.com>
2019-01-22 07:35:24 -0600

    feat(Page Container): Add Page Container component for background

---
+++ src/components/PageContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+const PageContainer = styled.main`
+  background-color: #33313b;
+  min-height: 100vh;
+  padding: 40px 5vw;
+  margin: 0 auto;
+`
+
+export default PageContainer
