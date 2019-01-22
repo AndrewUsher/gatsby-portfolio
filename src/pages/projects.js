ac030d4f4f964452b86a043916cd6c912096569c
Andrew Usher <andrewusher00@gmail.com>
2019-01-22 07:36:47 -0600

    feat(Projects): Add projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+  background-color: #000;
+`
+
+const Projects = () => <Section>Projects</Section>
+
+export default Projects
